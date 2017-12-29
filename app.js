99ee7dde655dffd2020a611df71556689abe00f5
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-29 11:49:43 -0600

    selected variables

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+var xhrbtn = document.querySelector('#xhr');
+var fetchbtn = document.querySelector('#fetch');
+var jquerybtn = document.querySelector('#jquery');
+var axiosbtn = document.querySelector('#axios');
