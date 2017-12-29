11a695596dfd43b9a3e692309c541d4f32f64037
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-29 11:54:51 -0600

    added XHR

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
-
 var xhrbtn = document.querySelector('#xhr');
 var fetchbtn = document.querySelector('#fetch');
 var jquerybtn = document.querySelector('#jquery');
 var axiosbtn = document.querySelector('#axios');
+var display = document.querySelector('#quote');
+
+xhrbtn.addEventListener('click', function() {
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function() {
+        if (XHR.readyState == 4 && XHR.status == 200) {
+            var quote = JSON.parse(XHR.responseText)[0];
+            display.innerText = quote;
+        }
+    }
+    XHR.open('GET', url);
+    XHR.send();
+});
